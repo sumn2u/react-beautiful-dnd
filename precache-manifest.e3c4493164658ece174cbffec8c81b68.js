f3859233a184050deab7fc848e6d398d8bf0cb1f
suman <suman.kunwar@daliaresearch.com>
2019-07-14 02:52:18 +0200

    Updates

---
+++ precache-manifest.e3c4493164658ece174cbffec8c81b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69485e24ecfefb48b0cc30ec2ce83337",
+    "url": "/react-beautiful-dnd/index.html"
+  },
+  {
+    "revision": "0c2570fba56533d09d6b",
+    "url": "/react-beautiful-dnd/static/css/2.d5794a55.chunk.css"
+  },
+  {
+    "revision": "0c2570fba56533d09d6b",
+    "url": "/react-beautiful-dnd/static/js/2.edba404c.chunk.js"
+  },
+  {
+    "revision": "04f4843237bf68eed5cd",
+    "url": "/react-beautiful-dnd/static/js/main.0c7cf280.chunk.js"
+  },
+  {
+    "revision": "4043530b7129eca06d69",
+    "url": "/react-beautiful-dnd/static/js/runtime~main.3bc74610.js"
+  }
+]);
